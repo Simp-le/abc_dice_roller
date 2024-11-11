8898aa1f6040397226427fa324b18e313d1f6955
mirro <mirrordeni@gmail.com>
2024-11-11 19:41:28 +0400

    Initial commit

---
+++ app/src/main/java/com/abc/diceroller/MainActivity.kt
@@ -0,0 +1,78 @@
+package com.abc.diceroller
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.abc.diceroller.ui.theme.DiceRollerTheme
+import java.security.InvalidParameterException
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            DiceRollerTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    DiceRollerApp(
+                        modifier = Modifier
+                            .padding(innerPadding)
+                            .fillMaxSize()
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun DiceWithButtonAndImage(modifier: Modifier = Modifier) {
+    var result by remember { mutableIntStateOf(1) }
+    val imageResource = when (result) {
+        1 -> R.drawable.dice_1
+        2 -> R.drawable.dice_2
+        3 -> R.drawable.dice_3
+        4 -> R.drawable.dice_4
+        5 -> R.drawable.dice_5
+        6 -> R.drawable.dice_6
+        else -> throw InvalidParameterException("No such parameter variant")
+    }
+
+    Column(modifier = modifier, horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(painter = painterResource(imageResource), contentDescription = result.toString())
+
+        Button(onClick = { result = (1..6).random() }) {
+            Text(stringResource(id = R.string.roll), fontSize = 24.sp)
+        }
+    }
+}
+
+
+@Preview
+@Composable
+fun DiceRollerApp(modifier: Modifier = Modifier) {
+    DiceWithButtonAndImage(
+        modifier
+            .fillMaxSize()
+            .wrapContentSize(Alignment.Center)
+    )
+}
